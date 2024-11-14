e4b4d3aa1ac4ec6811b0488703700a30e8e33aee
Lucas Guedes <dev.guedes27@gmail.com>
2024-11-14 09:52:56 -0300

    Create Models/Migrations Episodes/Seasons
    Criação das models e das migrations para episodios e temporadas das series.

--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Serie extends Model
 {
     use HasFactory;
     protected $fillable = ['nome'];
+
+
+    public function temporadas () {
+
+        return $this->hasMany(Season::class, 'series_id');
+
+    }
 }
